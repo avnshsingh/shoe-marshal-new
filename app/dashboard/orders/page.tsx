098d5842f2d24cd7f30aef3dd3341b956246e5b5
Avinash Singh <avinashsingh888555@gmail.com>
2024-11-25 11:54:41 +0530

    finished orders page UI

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -1,7 +1,75 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import React from "react";
 
 const OrdersPage = () => {
-  return <div>OrdersPage</div>;
+  return (
+    <Card>
+      <CardHeader className="px-7">
+        <CardTitle>Orders</CardTitle>
+        <CardDescription>Recent orders from your stores</CardDescription>
+      </CardHeader>
+      <CardContent className="px-7">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Customer</TableHead>
+              <TableHead>Type</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {[
+              {
+                id: 1,
+                User: {
+                  firstName: "John",
+                  lastName: "Doe",
+                  email: "bVHv2@example.com",
+                },
+                status: "pending",
+                createdAt: new Date(),
+                amount: 100,
+              },
+            ].map(item => (
+              <TableRow key={item.id}>
+                <TableCell>
+                  <p className="font-medium">{item.User?.firstName}</p>
+                  <p className="hidden md:flex text-sm text-muted-foreground">
+                    {item.User?.email}
+                  </p>
+                </TableCell>
+                <TableCell>Order</TableCell>
+                <TableCell>{item.status}</TableCell>
+                <TableCell>
+                  {new Intl.DateTimeFormat("en-US").format(item.createdAt)}
+                </TableCell>
+                <TableCell className="text-right">
+                  ${new Intl.NumberFormat("en-US").format(item.amount / 100)}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default OrdersPage;
